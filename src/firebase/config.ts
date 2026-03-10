1f02c934e5216a76c15f742cf035b4b161366faa
azyshu <azyshu7@gmail.com>
2026-03-10 19:06:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2245125114-226be",
+  "appId": "1:417650891730:web:beec16d8120caece32860b",
+  "apiKey": "AIzaSyBGNuxDju-Jw__1afiyx0b095b0r2hjUKY",
+  "authDomain": "studio-2245125114-226be.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "417650891730"
+};
